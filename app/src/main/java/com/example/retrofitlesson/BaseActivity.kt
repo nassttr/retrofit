bdaada2df05d52c07be4fef82ac3eb80bcd0f247
student <d@gmail.com>
2023-11-27 13:54:39 +0500

    retrofit

---
+++ app/src/main/java/com/example/retrofitlesson/BaseActivity.kt
@@ -0,0 +1,21 @@
+package com.example.retrofitlesson
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.retrofitlesson.databinding.ContentBaseBinding
+
+class BaseActivity : AppCompatActivity() {
+
+    private lateinit var binding: ContentBaseBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ContentBaseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/example/retrofitlesson/LoginViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.retrofitlesson
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+class LoginViewModel : ViewModel() {
+    val token = MutableLiveData<String>()
+}
